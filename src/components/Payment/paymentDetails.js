15d22cc19a1ee63d9d8a0e9d90ff308d1f8fa406
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-01-21 01:47:12 +0500

    paymentDetails added

--- src/components/Payment/paymentDetails.js
+++ src/components/Payment/paymentDetails.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router';
-import './paymentType.css'
+import './paymentDetails.css'
 import Layout from '../Layout/Layout'
 
 function PaymentDetails() {
-  const [selectedPaymentType, setSelectedPaymentType] = useState('');
+  const [selectedRelation, setSelectedRelation] = useState('');
 
-  const handlePaymentTypeChange = (event) => {
-    setSelectedPaymentType(event.target.value);
+  const handleRelationChange = (event) => {
+    setSelectedRelation(event.target.value);
   };
 
   const handleSubmit = () => {
-    // Add your payment processing logic here based on selectedPaymentType
-    console.log('Processing payment with type:', selectedPaymentType);
+    console.log('Processing relation with type:', selectedRelation);
   };
 
   return (
@@ -20,29 +19,42 @@ function PaymentDetails() {
         <div class="payment-header container h-0 w-75">
             <h1>Online Payment</h1>
         </div>
-        <div class="payment-options container">
-            <p><label htmlFor="studentName">Student Name:</label></p>
-            <input id="studentName"/>
-            <p><label htmlFor="studentName">Student Name:</label></p>
-            <input id="studentName"/>
-            <p><label htmlFor="studentName">Student Name:</label></p>
-            <input id="studentName"/>
-            <p><label htmlFor="studentName">Student Name:</label></p>
-            <input id="studentName"/>
-            <p><label htmlFor="studentName">Student Name:</label></p>
-            <input id="studentName"/>
-            <p><label htmlFor="studentName">Student Name:</label></p>
-            <input id="studentName"/>
-            <div class="text-center"><h4><label htmlFor="paymentType">Payment Type</label></h4></div>
+        <div class="container">
+            <div class="row col-lg-5">
+                <p><label htmlFor="studentName">Student Name:</label></p>
+                <input type="text" class="form-control" id="studentName"/>
+                <p><label htmlFor="studentRegNo" required="true">Student Registration No:</label></p>
+                <input type="text" class="form-control" id="studentRegNo"/>
+            </div>
+            <div class="row col-lg-5">
+                <p><label htmlFor="studentEmail">Student Email:</label></p>
+                <input type="text" class="form-control" id="studentEmail"/>
+                <p><label htmlFor="studentCNIC">Student CNIC:</label></p>
+                <input type="text" class="form-control" id="studentCNIC"/>
+            </div>
+            <div class="row col-lg-5">
+                <p><label htmlFor="program">Programs (Degree):</label></p>
+                <input type="text" class="form-control" id="program"/>
+                <p><label htmlFor="term">Term:</label></p>
+                <input type="text" class="form-control" id="term"/>
+            </div>
+            <div class="row col-lg-5">
+                <p><label htmlFor="challanNo">Challan No:</label></p>
+                <input type="text" class="form-control" id="challanNo"/>
+                <p><label htmlFor="amount">Amount:</label></p>
+                <input type="text" class="form-control" id="amount"/>
+            </div>
+            <div class="text-center"><h4><label htmlFor="relation">Relation</label></h4></div>
             <form>
-              <select class="form-select" name="paymentType" id="paymentType" value={selectedPaymentType} onChange={handlePaymentTypeChange}>
-                  <option value="">Select Payment Type</option>
-                  <option value="fee">Tuition Fee, Hostel Charges, and University Dues</option>
-                  <option value="testFee">Test Fee</option>
-                  <option value="clearanceFee">Clearance Fee</option>
-              </select>
+                <select class="form-select" name="relation" id="relation" value={selectedRelation} onChange={handleRelationChange}>
+                    <option value="">Select Relationship with Student</option>
+                    <option value="self">Self</option>
+                    <option value="father">Father</option>
+                    <option value="brother">Brother</option>
+                    <option value="mother">Mother</option>
+                </select>
             </form>
-            <button class="btn btn-dark" type="button" onClick={<Navigate to='/payment-details' />}>Submit</button>
+            <button class="btn btn-dark" type="button" onClick={< Navigate to='/' />}>Submit</button>
         </div>
     </Layout>
   );
