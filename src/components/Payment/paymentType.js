93d4274103d4e8800766d47b8d1e56a115a0cb53
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-01-30 13:46:00 +0500

    updated paymentType Navigate parameters

--- src/components/Payment/paymentType.js
+++ src/components/Payment/paymentType.js
@@ -3,14 +3,21 @@ import { Navigate } from 'react-router';
 import Layout from '../Layout/Layout'
 
 function PaymentType() {
+  const [selectedPaymentTypeId, setSelectedPaymentTypeId] = useState(0);
   const [selectedPaymentType, setSelectedPaymentType] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
   const handlePaymentTypeChange = (event) => {
-    setSelectedPaymentType(event.target.value);
+    const selectedIndex = event.target.selectedIndex;
+    setSelectedPaymentTypeId(Number(event.target.value));
+    setSelectedPaymentType(event.target.options[selectedIndex].text);
   };
 
   const handleSubmit = () => {
-    console.log('Processing payment with type:', selectedPaymentType);
+    if (selectedPaymentTypeId === 0) {
+      alert('Please select a Payment Type first')
+    }
+    else {setRedirect(true)}
   };
 
   return (
@@ -20,15 +27,16 @@ function PaymentType() {
       </div>
       <div class="text-center container">
         <h4><label htmlFor="paymentType">Payment Type</label></h4>
-        <select class="form-select" name="paymentType" id="paymentType" value={selectedPaymentType} onChange={handlePaymentTypeChange}>
-          <option value="">Select Payment Type</option>
-          <option value="fee">Tuition Fee, Hostel Charges, and University Dues</option>
-          <option value="testFee">Test Fee</option>
-          <option value="clearanceFee">Clearance Fee</option>
+        <select class="form-select" name="paymentType" id="paymentType" value={selectedPaymentTypeId} onChange={handlePaymentTypeChange}>
+          <option value="0">Select Payment Type</option>
+          <option value="1">Tuition Fee, Hostel Charges, and University Dues</option>
+          <option value="2">Test Fee</option>
+          <option value="3">Clearance Fee</option>
         </select>
         <div className='d-flex justify-content-center'>
-          <button class="btn w-50 btn-dark" type="button" onClick={< Navigate to='/payment-details' />}>Submit</button>
+          <button class="btn w-50 btn-dark" type="button" onClick={handleSubmit}>Submit</button>
         </div>
+        {redirect && < Navigate to='/payment-details' state={{id: selectedPaymentTypeId, text: selectedPaymentType}} />}
       </div>
     </Layout>
   );
