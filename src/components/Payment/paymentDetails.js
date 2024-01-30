f079538d0ed1c8496d08d64d645bfa56641b6e4b
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-01-30 16:18:03 +0500

    swapped screens order

--- src/components/Payment/paymentDetails.js
+++ src/components/Payment/paymentDetails.js
@@ -1,106 +1,67 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import './paymentDetails.css'
 import Layout from '../Layout/Layout'
 
 function PaymentDetails() {
     const [selectedRelation, setSelectedRelation] = useState('');
-    const [Amount, setAmount] = useState(0);
-    const location = useLocation();
-    const { id, text } = location.state;
+    const [redirect, setRedirect] = useState(false);
 
     const handleRelationChange = (event) => {
         setSelectedRelation(event.target.value)
     };
 
-    const handleAmountChange = (event) => {
-        setAmount(Number(event.target.value))
-    };
-
-    const handleBack = () => {};
-
-    const handleProceed = () => {
-        fetch('http://localhost:3001/create-checkout-session', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                amount: Amount,
-                id: id,
-                paymentType: text,
-            })
-        }).then(res => {
-            if (res.ok) return res.json()
-            return res.json().then(json => Promise.reject(json))
-        }).then(({ url }) => {
-            // console.log(Amount, text)
-            window.location = url
-        }).catch(e => {
-            console.error(e.error)
-        })
+    const handleSubmit = () => {
+        setRedirect(true)
     };
 
     return (
         <Layout>
-
-            <div class="text-center container">
+            <div className="text-center container">
                 <h1>Online Payment</h1>
             </div>
-            <div class="container mt-5">
-                <div class="row details-container">
+            <div className="container mt-5">
+                <div className="row details-container">
                     <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="studentName" placeholder='name'/>
+                        <input type="text" className="form-control" id="studentName" placeholder='name'/>
                         <label for="studentName">Student Name:</label>
                     </div>
                     <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="studentRegNo" placeholder='name'/>
-                        <label for="studentRegNo" required="true">Student Registration No:</label>
+                        <input type="text" className="form-control" id="studentRegNo" placeholder='name'/>
+                        <label for="studentRegNo">Student Registration No:</label>
                     </div>
                     <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="studentEmail" placeholder='name'/>
+                        <input type="text" className="form-control" id="studentEmail" placeholder='name'/>
                         <label for="studentEmail">Student Email:</label>
                     </div>
                     <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="studentCNIC" placeholder='name'/>
+                        <input type="text" className="form-control" id="studentCNIC" placeholder='name'/>
                         <label for="studentCNIC">Student CNIC:</label>
                     </div>
                     <div className='mb-3'>
                         <span className='input-group-text text-center'>Payment secured with STRIPE via Askari and Habib bank ltd.</span>
                     </div>
                     <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="program" placeholder='name'/>
+                        <input type="text" className="form-control" id="program" placeholder='name'/>
                         <label for="program">Programs (Degree):</label>
                     </div>
                     <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="term" placeholder='name'/>
+                        <input type="text" className="form-control" id="term" placeholder='name'/>
                         <label for="term">Term:</label>
                     </div>
-                    <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="challanNo" placeholder='name'/>
-                        <label for="challanNo">Challan No:</label>
-                    </div>
-                    <div className='col-lg-6 mb-3 form-floating'>
-                        <input type="text" class="form-control" id="amount" placeholder='name' onChange={handleAmountChange}/>
-                        <label for="amount">Amount:</label>
-                    </div>
                 </div>
-                <div class="text-center mt-3"><h4><label htmlFor="relation">Relation</label></h4></div>
-                <select class="form-select" name="relation" id="relation" value={selectedRelation} onChange={handleRelationChange}>
+                <div className="text-center mt-3"><h4><label htmlFor="relation">Relation</label></h4></div>
+                <select className="form-select" name="relation" id="relation" value={selectedRelation} onChange={handleRelationChange}>
                     <option value="">Select Relationship with Student</option>
                     <option value="self">Self</option>
                     <option value="father">Father</option>
                     <option value="brother">Brother</option>
                     <option value="mother">Mother</option>
                 </select>
-                <div className="d-flex justify-content-center mt-3">
-                    <button className="btn btn-secondary me-2" type="button" onClick={handleBack}>
-                        Back
-                    </button>
-                    <button className="btn btn-primary" type="button" onClick={handleProceed}>
-                        Proceed
-                    </button>
+                <div className='d-flex justify-content-center'>
+                    <button class="btn w-50 btn-dark" type="button" onClick={handleSubmit}>Submit</button>
                 </div>
+                { redirect && < Navigate to='/'/> }
             </div>
         </Layout>
     );
--- src/components/Payment/paymentType.js
+++ src/components/Payment/paymentType.js
@@ -3,9 +3,9 @@ import { Navigate } from 'react-router';
 import Layout from '../Layout/Layout'
 
 function PaymentType() {
+  const [Amount, setAmount] = useState(0);
   const [selectedPaymentTypeId, setSelectedPaymentTypeId] = useState(0);
   const [selectedPaymentType, setSelectedPaymentType] = useState('');
-  const [redirect, setRedirect] = useState(false);
 
   const handlePaymentTypeChange = (event) => {
     const selectedIndex = event.target.selectedIndex;
@@ -13,11 +13,32 @@ function PaymentType() {
     setSelectedPaymentType(event.target.options[selectedIndex].text);
   };
 
-  const handleSubmit = () => {
-    if (selectedPaymentTypeId === 0) {
-      alert('Please select a Payment Type first')
-    }
-    else {setRedirect(true)}
+  const handleAmountChange = (event) => {
+    setAmount(Number(event.target.value))
+  };
+
+  const handleBack = () => {};
+
+  const handleProceed = () => {
+      fetch('http://localhost:3001/create-checkout-session', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              amount: Amount,
+              id: selectedPaymentTypeId,
+              paymentType: selectedPaymentType,
+          })
+      }).then(res => {
+          if (res.ok) return res.json()
+          return res.json().then(json => Promise.reject(json))
+      }).then(({ url }) => {
+          // console.log(Amount, text)
+          window.location = url
+      }).catch(e => {
+          console.error(e.error)
+      })
   };
 
   return (
@@ -25,18 +46,32 @@ function PaymentType() {
       <div class="text-center container mb-5">
         <h1>Online Payment</h1>
       </div>
-      <div class="text-center container">
-        <h4><label htmlFor="paymentType">Payment Type</label></h4>
-        <select class="form-select" name="paymentType" id="paymentType" value={selectedPaymentTypeId} onChange={handlePaymentTypeChange}>
-          <option value="0">Select Payment Type</option>
-          <option value="1">Tuition Fee, Hostel Charges, and University Dues</option>
-          <option value="2">Test Fee</option>
-          <option value="3">Clearance Fee</option>
-        </select>
-        <div className='d-flex justify-content-center'>
-          <button class="btn w-50 btn-dark" type="button" onClick={handleSubmit}>Submit</button>
-        </div>
-        {redirect && < Navigate to='/payment-details' state={{id: selectedPaymentTypeId, text: selectedPaymentType}} />}
+      <div class="text-center container">  
+          <h4><label htmlFor="paymentType">Payment Type</label></h4>
+          <select class="form-select" name="paymentType" id="paymentType" value={selectedPaymentTypeId} onChange={handlePaymentTypeChange}>
+            <option value="0">Select Payment Type</option>
+            <option value="1">Tuition Fee, Hostel Charges, and University Dues</option>
+            <option value="2">Test Fee</option>
+            <option value="3">Clearance Fee</option>
+          </select>
+          <div className="row details-container mt-3">
+              <div className='col-lg-6 mb-3 form-floating'>
+                  <input type="text" className="form-control" id="challanNo" placeholder='name'/>
+                  <label for="challanNo">Challan No:</label>
+              </div>
+              <div className='col-lg-6 mb-3 form-floating'>
+                  <input type="text" className="form-control" id="amount" placeholder='name' onChange={handleAmountChange}/>
+                  <label for="amount">Amount:</label>
+              </div>
+          </div>
+          <div className="d-flex justify-content-center">
+              <button className="btn btn-secondary me-2" type="button" onClick={handleBack}>
+                  Back
+              </button>
+              <button className="btn btn-primary" type="button" onClick={handleProceed}>
+                  Proceed
+              </button>
+          </div>
       </div>
     </Layout>
   );
