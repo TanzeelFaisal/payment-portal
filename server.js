649b3e5848c80f556c6ca2ba5b78a81cd81048ae
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-01-31 15:56:19 +0500

    added api endpoints

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 const express = require('express')
+const { ObjectId } = require('mongodb')
+const { connectDb, getDb } = require('./mongodb')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
@@ -9,12 +11,67 @@ app.use(cors({
 }))
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+let db = null
 
 const minAmount = new Map([
     [1, { priceInPaisas: 2000000, name: "Tuition Fee, Hostel Charges, and University Dues" }],
     [2, { priceInPaisas: 60000, name: "Test Fee" }],
     [3, { priceInPaisas: 600000, name: "Clearance Fee" }],
 ])
+
+connectDb(err => {
+    if (!err) {
+        app.listen(3001)
+        db = getDb()
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        console.log('Attempting login with email:', email);
+        const user = await db.collection('users').findOne({ studentEmail: email });
+        console.log('Retrieved user:', user);
+        if (!user) {
+            return res.status(404).json({ error: 'User doesn\'t exist' });
+        }
+        if (user.password !== password) {
+            return res.status(401).json({ error: 'Incorrect email or password' });
+        }
+        res.status(200).json({ message: 'Login successful', user: user });
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.post('/submit-payment-details', async (req, res) => {
+    const { studentName, studentRegNo, studentEmail, studentCNIC, password, program, term, selectedRelation } = req.body;
+
+    try {
+        const existingUser = await db.collection('users').findOne({ $or: [{ studentCNIC }, { studentEmail }, { studentRegNo }] });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Student CNIC, Email, or Registration No already exists' });
+        }
+
+        const result = await db.collection('users').insertOne({
+            studentName,
+            studentRegNo,
+            studentEmail,
+            studentCNIC,
+            password,
+            program,
+            term,
+            selectedRelation,
+            approved_status: false
+        });
+
+        res.status(201).json({ message: 'Payment details submitted successfully', user: result });
+    } catch (error) {
+        console.error('Error submitting payment details:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.post('/create-checkout-session', async (req, res) => {
     try {
@@ -31,13 +88,11 @@ app.post('/create-checkout-session', async (req, res) => {
                 },
                 quantity: 1
             }],
-            success_url: `${process.env.CLIENT_URL}/success`,
-            cancel_url: `${process.env.CLIENT_URL}/cancel`
+            success_url: `${process.env.CLIENT_URL}/select-payment`,
+            cancel_url: `${process.env.CLIENT_URL}/select-payment`
         })
         res.json({url: session.url})
-    } catch(e) {
-        res.status(500).json({error: e.message})
+    } catch(error) {
+        res.status(500).json({error: error.message})
     }
 })
-
-app.listen(3001)
