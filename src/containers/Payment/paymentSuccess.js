5be24e1af5f63224c5e1f2bad94d00103aedb870
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-01-31 19:54:53 +0500

    paymentSuccess page after successful stripe payment

---
+++ src/containers/Payment/paymentSuccess.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import Layout from '../../components/Layout/Layout';
+
+function PaymentSuccess() {
+  useEffect(() => {
+    savePaymentData();
+  }, []);
+
+  const savePaymentData = async () => {
+    try {
+      const userData = JSON.parse(localStorage.getItem('userData'));
+
+      const response = await fetch('http://localhost:3001/save-payment-data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          studentName: userData.user.studentName,
+          studentRegNo: userData.user.studentRegNo,
+          studentEmail: userData.user.studentEmail,
+          amount: userData.user.amount,
+          paymentType: userData.user.paymentType,
+          dateTime: new Date(),
+        }),
+      });
+      if (response.ok) {
+        console.log('Payment details saved successfully');
+      } else {
+        console.error('Failed to save payment data:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error saving payment data:', error);
+    }
+  };
+
+  return (
+    <Layout>
+      <div className="container mt-5">
+        <div className="alert alert-success" role="alert">
+          Payment was successful!
+        </div>
+        <button className="btn btn-primary" onClick={() => window.location = '/select-payment'}>
+          Back to Payment
+        </button>
+      </div>
+    </Layout>
+  );
+}
+
+export default PaymentSuccess;
