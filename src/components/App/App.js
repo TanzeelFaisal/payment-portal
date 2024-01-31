70be40376d5c591ec08a4233f62a96ec5e118fa6
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-01-31 19:45:09 +0500

    approve, payment success routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../../containers/Home/home'
-import PaymentType from '../../containers/PaymentType/paymentType'
-import PaymentDetails from '../../containers/PaymentDetails/paymentDetails'
+import Approve from '../../containers/Admin/admin-approval'
+import PaymentType from '../../containers/Payment/paymentType'
+import PaymentDetails from '../../containers/Payment/paymentDetails'
+import PaymentSuccess from '../../containers/Payment/paymentSuccess'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={< Home />} />
+          <Route path="/admin-approval" element={< Approve />} />
           <Route path="/select-payment" element={< PaymentType />} />
           <Route path="/payment-details" element={< PaymentDetails />} />
+          <Route path="/payment-success" element={< PaymentSuccess />} />
         </Routes>
     </BrowserRouter>
   );
